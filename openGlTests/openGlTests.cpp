ea9391804b00a948d8b13b2fc6e5267c951d7631
DaMrStick <76888971+DaMrStick@users.noreply.github.com>
2025-02-19 19:06:26 +1100

    added some basic zoom perspective stuff

--- openGlTests/openGlTests.cpp
+++ openGlTests/openGlTests.cpp
@@ -1,5 +1,7 @@
 #include <GLFW/glfw3.h>
+//#include <GLFW
 #include <iostream>
+#include <vector>
 
 #define ZOOM_STRENGTH 0.2
 
@@ -30,6 +32,11 @@ struct vector2
 	vector2 operator/(vector2 other) const
 	{
 		return vector2(x / other.x, y / other.y);
+	}
+
+	vector2 operator+(float other) const
+	{
+		return vector2(x+other, y+other);
 	}
 
 	vector2 operator*(float other) const
@@ -66,17 +73,6 @@ struct vector3
 	{}
 };
 
-struct extraData
-{
-	float zoom = 1;
-	vector2 offset = vector2(0, 0);
-	vector2 screenSize = vector2(0, 0);
-	bool mouseLeftHeld = false;
-	vector2 mousePositionOld = vector2(0, 0);
-
-	extraData()
-	{}
-};
 
 void DrawSquare(vector2 v1, vector2 v2, vector3 color)
 {
@@ -96,11 +92,14 @@ public:
 	vector2 offset = vector2(0,0);
 	vector2 pos = vector2(0,0);
 	vector2 size = vector2(0,0);
+	vector2 offsetExtra = vector2(0,0);
 
 public:
 	agent();
 
 	void render();
+
+	vector2 getSPos(unsigned int screenWidth, unsigned int screenHeight);
 };
 
 agent::agent()
@@ -109,14 +108,58 @@ agent::agent()
 
 void agent::render()
 {
-	DrawSquare(pos + offset - size/2, pos + offset + size/2, color);
+	DrawSquare(pos + (offset + offsetExtra) - size/2, pos + (offset + offsetExtra) + size/2, color);
+	// printf("offset dx %f, offset dy %f, offsetEx %f, offsetEy %f \n", offset.x, offset.y, offsetExtra.x, offsetExtra.y);
 }
+
+vector2 agent::getSPos(const unsigned int screenWidth, const unsigned int screenHeight)
+{
+	return vector2((pos.x + 1) * screenWidth, (pos.y + 1) * screenHeight);
+}
+
+
+
+struct extraData
+{
+	float zoom = 1;
+	vector2 offset = vector2(0, 0);
+	vector2 screenSize = vector2(0, 0);
+	bool mouseLeftHeld = false;
+	vector2 mousePositionOld = vector2(0, 0);
+
+	std::vector<agent*> agents;
+
+	extraData()
+	{}
+};
 
 void scrollCallBack(GLFWwindow* window, double xOffset, double yOffset)
 {
 	void* exDataP = glfwGetWindowUserPointer(window);
 	extraData* dataP = (extraData*)exDataP;
 	dataP->zoom = std::max(dataP->zoom + yOffset * ZOOM_STRENGTH, 0.1);
+
+	for (int i = 0; i < dataP->agents.size(); i++)
+	{
+		agent* obj = dataP->agents[i];
+		vector2 objSPos = obj->getSPos(dataP->screenSize.x, dataP->screenSize.y);
+		vector2 mousePos(0, 0);
+		double mouseX; double mouseY;
+		glfwGetCursorPos(window, &mouseX,&mouseY);
+		mousePos.x = mouseX;
+		mousePos.y = mouseY;
+		vector2 vecToMouse = mousePos - objSPos;
+		// std::cout << "offset pre: " << obj->offsetExtra.x << std::endl;
+		// obj->offsetExtra = obj->offsetExtra + vecToMouse * 0.0001 * std::max(yOffset * ZOOM_STRENGTH, 0.1);
+		obj->offsetExtra = obj->offsetExtra + (vecToMouse * (0.0001 * yOffset * ZOOM_STRENGTH * -1));
+		// std::cout << (obj->offsetExtra + vecToMouse * (yOffset * ZOOM_STRENGTH * -1)).x << " , " << (obj->offsetExtra + vecToMouse * (5yOffset * ZOOM_STRENGTH * -1)).y << std::endl;
+		// obj->offsetExtra = vector2(1000, 1000);
+		// std::cout << "offset post: " << obj->offsetExtra.x << std::endl;
+		// std::cout << "offset post: " << &obj << std::endl;
+		// std::cout << obj.offset.x << " " << obj.offset.y << std::endl;
+		// std::cout << (vecToMouse * std::max(yOffset * ZOOM_STRENGTH, 0.1)).x << " " << (vecToMouse * std::max(yOffset * ZOOM_STRENGTH, 0.1)).y<< std::endl;
+	}
+
 }
 
 void mouseButtonCallBack(GLFWwindow* window, int button, int action, int mods)
@@ -185,6 +228,16 @@ int main()
 	AI.pos = vector2(0, 0);
 	AI.size = vector2((size * data.zoom)/width, (size * data.zoom)/height);
 
+	agent npc = agent();
+	npc.color = vector3(0, 1, 0);
+	npc.pos = vector2(0.5, 0);
+	npc.size = vector2((size * data.zoom)/width, (size * data.zoom)/height);
+
+	data.agents.push_back(&AI);
+	data.agents.push_back(&npc);
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
@@ -195,6 +248,11 @@ int main()
 		AI.offset = data.offset;
 
 
+		npc.size = vector2((size * data.zoom)/width, (size * data.zoom)/height);
+		npc.offset = data.offset;
+
+
+		npc.render();
 		AI.render();
 
 		// if (data.mouseLeftHeld)
